c9b9878cd531e41723a01baa0e763d8414815615
nglsz006 <nicolas.ibanez-l@mail.escuelaing.edu.co>
2026-03-10 21:26:11 -0500

    scalefolding

---
+++ DOSW-Library/src/main/java/edu/eci/dosw/DOSW_Library/DoswLibraryApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.dosw.DOSW_Library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoswLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoswLibraryApplication.class, args);
+	}
+
+}
---
+++ DOSW-Library/src/test/java/edu/eci/dosw/DOSW_Library/DoswLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.dosw.DOSW_Library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoswLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
