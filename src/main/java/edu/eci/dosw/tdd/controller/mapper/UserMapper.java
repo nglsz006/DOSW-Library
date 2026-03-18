8b3ee5afd844cf92d4c62105d19c804c1a91e1d3
nglsz006 <nicolas.ibanez-l@mail.escuelaing.edu.co>
2026-03-17 21:46:23 -0500

    desarrollo de mappers y dtos

--- src/main/java/edu/eci/dosw/tdd/controller/mapper/UserMapper.java
+++ src/main/java/edu/eci/dosw/tdd/controller/mapper/UserMapper.java
@@ -1,4 +1,18 @@
 package edu.eci.dosw.tdd.controller.mapper;
 
+import edu.eci.dosw.tdd.controller.dto.UserDTO;
+import edu.eci.dosw.tdd.core.model.User;
+
 public class UserMapper {
+
+    private UserMapper() {
+    }
+
+    public static UserDTO toDTO(User user) {
+        return new UserDTO(user.getId(), user.getName());
+    }
+
+    public static User toModel(UserDTO dto) {
+        return new User(dto.getName(), dto.getId());
+    }
 }
