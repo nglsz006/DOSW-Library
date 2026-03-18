166fdef25ae887257f7c231eeb6b0ab8b62c328c
nglsz006 <nicolas.ibanez-l@mail.escuelaing.edu.co>
2026-03-17 20:47:41 -0500

    desarrollo de las clases de soporte como g eneración de IDs, manejo de fechas y validaciones generales

--- src/main/java/edu/eci/dosw/tdd/core/util/DateUtil.java
+++ src/main/java/edu/eci/dosw/tdd/core/util/DateUtil.java
@@ -1,4 +1,35 @@
 package edu.eci.dosw.tdd.core.util;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Utilidad para manejo de fechas en formato ISO (yyyy-MM-dd).
+ */
 public class DateUtil {
+
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    private DateUtil() {
+    }
+
+    public static String getCurrentDate() {
+        return LocalDate.now().format(FORMATTER);
+    }
+
+    public static String calculateReturnDate(int daysFromNow) {
+        return LocalDate.now().plusDays(daysFromNow).format(FORMATTER);
+    }
+
+    public static boolean isValidDate(String date) {
+        if (date == null || date.isBlank()) {
+            return false;
+        }
+        try {
+            LocalDate.parse(date, FORMATTER);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
