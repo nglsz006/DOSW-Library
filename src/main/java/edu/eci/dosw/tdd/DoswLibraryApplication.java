bc7e5ece8e0aab006edb8312b4238810db9b89a5
nglsz006 <nicolas.ibanez-l@mail.escuelaing.edu.co>
2026-03-26 22:08:02 -0500

    configuré la conexión a postgresql en application.yaml con hibernate ddl-auto update, también habilité swagger y agregué @EnableJpaRepositories y @EntityScan en el main

--- src/main/java/edu/eci/dosw/tdd/DoswLibraryApplication.java
+++ src/main/java/edu/eci/dosw/tdd/DoswLibraryApplication.java
@@ -2,8 +2,10 @@ package edu.eci.dosw.tdd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "edu.eci.dosw.tdd.persistence.repository")
 public class DoswLibraryApplication {
 
 	public static void main(String[] args) {
