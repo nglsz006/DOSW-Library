164f980b6c57d4668c6ced435866da06dd7fc51f
nglsz006 <nicolas.ibanez-l@mail.escuelaing.edu.co>
2026-03-26 21:57:17 -0500

    creé las interfaces de repositorio BookRepository, UserRepository y LoanRepository con los métodos de búsqueda personalizados que necesito para las consultas

---
+++ src/main/java/edu/eci/dosw/tdd/persistence/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package edu.eci.dosw.tdd.persistence.repository;
+
+import edu.eci.dosw.tdd.persistence.entity.BookEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
+
+public interface BookRepository extends JpaRepository<BookEntity, Long> {
+    Optional<BookEntity> findByTitleAndAuthor(String title, String author);
+    List<BookEntity> findByAvailableCopiesGreaterThan(int copies);
+}
